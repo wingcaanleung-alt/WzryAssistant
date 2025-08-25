4c0e362423b7f08d0f5a14abee2a032c9fa620b7
wingcaanleung-alt <wingcaanleung@gmail.com>
2025-08-25 13:58:03 +0800

    Create MainActivity.java

---
+++ app/src/main/java/com/example/wzryassistant/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.wzryassistant;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.btn_hello);
+        btn.setOnClickListener(v ->
+                Toast.makeText(this, "悬浮窗功能待实现", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
